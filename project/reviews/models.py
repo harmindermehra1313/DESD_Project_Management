f73ef226d878b69d247d73317bcdac6f5bae48dc
Oishik Biswas <pippal5536@gmail.com>
2026-02-19 17:59:12 +0000

    fixed model errors, set up mysql in django setting file, set auth user and made migrations.

--- project/reviews/models.py
+++ project/reviews/models.py
@@ -4,10 +4,10 @@ from django_mysql.models import EnumField #pip install django-mysql
 
 class Review(models.Model):
     id = models.AutoField(primary_key=True)
-    product_id = models.ForeignKey(Product, on_delete=models.CASCADE, db_column="product_id")
-    customer_id = models.ForeignKey(Customer, on_delete=models.CASCADE, db_column="customer_id")
-    order_id = models.ForeignKey(Order, on_delete=models.CASCADE, db_column="order_id")
-    moderated_by_admin_id = models.ForeignKey(Admin, on_delete=models.CASCADE, db_column="admin_id", null=True)
+    product_id = models.ForeignKey("products.Product", on_delete=models.CASCADE, db_column="product_id")
+    customer_id = models.ForeignKey("accounts.Customer", on_delete=models.CASCADE, db_column="customer_id")
+    order_id = models.ForeignKey("orders.Order", on_delete=models.CASCADE, db_column="order_id")
+    moderated_by_admin_id = models.ForeignKey("accounts.Admin", on_delete=models.CASCADE, db_column="admin_id", null=True)
     rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
     title = models.CharField(max_length=255)
     text = models.TextField()
@@ -19,8 +19,9 @@ class Review(models.Model):
 class ReviewResponse(models.Model):
     id = models.AutoField(primary_key=True)
     review_id = models.ForeignKey(Review, on_delete=models.CASCADE, db_column="review_id")
-    producer_id = models.ForeignKey(Producer, on_delete=models.CASCADE, db_column="producer_id")
-    moderated_by_admin_id = models.ForeignKey(AdmintionLog, on_delete=models.CASCADE, db_column="moderated_by_admin_id", null=True)
+    producer_id = models.ForeignKey("accounts.Producer", on_delete=models.CASCADE, db_column="producer_id")
+    # moderated_by_admin_id = models.ForeignKey("admin_records.AdmintionLog", on_delete=models.CASCADE, db_column="moderated_by_admin_id", null=True)
+    moderated_by_admin_id = models.ForeignKey("accounts.Admin", on_delete=models.CASCADE, db_column="moderated_by_admin_id", null=True)
     response_text = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     status = EnumField(choices=['PUBLISHED', 'HIDDEN', 'FLAGGED', 'REMOVED'])
