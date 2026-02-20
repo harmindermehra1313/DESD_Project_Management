e1cf23435be17678c13273743a99ae2ba0469bea
Harminder Singh <Harminder2.Singh@live.uwe.ac.uk>
2026-02-20 11:07:26 +0000

    Resiger and Login Forms

--- project/accounts/views.py
+++ project/accounts/views.py
@@ -1,19 +1,22 @@
 from django.shortcuts import render
 
 # Create your views here.
-def index(request):
-    return render(request, "accounts/index.html")
+def register(request):
+    return render(request, "accounts/register.html")
+
+def login(request):
+    return render(request, "accounts/login.html")
 
 # TBC - loads placeholder form based on selected role but doesn't save anything
-def register(request):
-    role = request.GET.get("role", "customer")
+# def register(request):
+#     role = request.GET.get("role", "customer")
 
-    if request.method == "POST":
-        # TBC - save, currently print
-        print("Received POST for role:", request.POST.get("role"))
-        print("Form data:", request.POST)
-        return render(request, "accounts/register_success.html")
+#     if request.method == "POST":
+#         # TBC - save, currently print
+#         print("Received POST for role:", request.POST.get("role"))
+#         print("Form data:", request.POST)
+#         return render(request, "accounts/register_success.html")
 
-    return render(request, "accounts/register.html", {
-        "role": role,
-    })
+#     return render(request, "accounts/register.html", {
+#         "role": role,
+#     })
