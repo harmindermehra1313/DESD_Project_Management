e33f9ef4773832aa4e6a9614de10590fe3b892ed
hj2-higgs <Hannah2.Higgs@live.uwe.ac.uk>
2026-02-24 19:19:36 +0000

    Fixed model issues. Added checkout screen and order confirmation currently not finished.

--- project/orders/urls.py
+++ project/orders/urls.py
@@ -4,5 +4,8 @@ from . import views
 app_name = "orders"
 
 urlpatterns = [
-    path("", views.index, name='index')
+    path("", views.index, name='index'),
+    path("fake-add-to-cart/", views.fake_add_to_cart, name="fake_add_to_cart"),
+    path("checkout/", views.checkout, name="checkout"),
+    path("success/<str:reference>/", views.order_success, name="order_success"),
 ]
