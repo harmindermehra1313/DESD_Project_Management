d8a693025377164e8295b8bdea03f2fc038e5f93
joemoron123 <joe2.morton@live.uwe.ac.uk>
2026-02-28 13:52:12 +0000

    add products page done along with some changes to products db

--- project/products/models.py
+++ project/products/models.py
@@ -4,8 +4,22 @@ from django.contrib.postgres.fields import ArrayField
 
 
 class Category(models.Model):
+    class FoodGroups(models.TextChoices):
+        MEAT = 'MT', 'Meat'
+        DAIRY_AND_EGGS = 'DAE', 'Dairy and Eggs'
+        FRUIT = 'FR', 'Fruit'
+        VEGETABLES = 'VEG', 'Vegetables'
+        SEASONAL = 'SEA', 'Seasonal'
+
     name = models.CharField(
         max_length=100
+    )
+
+    food_groups = models.CharField(
+        max_length = 20,
+        choices = FoodGroups.choices,
+        default=FoodGroups.SEASONAL
+
     )
 
     description = models.TextField(
@@ -98,8 +112,8 @@ class Product(models.Model):
         default = Unit.EACH
     )
 
-    image = models.CharField(
-        max_length = 255
+    image = models.ImageField(
+        upload_to = 'products/'
     )
 
     stock_quantity = models.IntegerField(
@@ -110,9 +124,7 @@ class Product(models.Model):
         default = 0
     )
     
-    harvest_date = models.DateTimeField(
-        auto_now_add = True
-    )
+    harvest_date = models.DateTimeField()
 
     farm_origin = models.CharField(
         max_length = 150
