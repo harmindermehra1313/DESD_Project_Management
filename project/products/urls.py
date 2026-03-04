bd64a262ab967208fc2aadd5112428053f0c899d
Oishik <pippal5536@gmail.com>
2026-03-04 13:24:50 +0000

    latest changes from main

--- project/products/urls.py
+++ project/products/urls.py
@@ -7,9 +7,6 @@ from .views import ProductDetailView
 urlpatterns = [
     path('', views.product_list, name='products_list'),
     path('add/', views.add_product, name='add_product'),
-    # path('<int:product_id>/', views.product_detail, name='product_detail'),
-    path('cart/add/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
-    path("<int:pk>/", ProductDetailView.as_view(), name="detail"),  # /products/<pk>/
     # path('cart/add/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
     path("<int:pk>/", ProductDetailView.as_view(), name="detail"),  # /products/<pk>/
     # Harminder Edits
--- project/products/views.py
+++ project/products/views.py
@@ -7,7 +7,6 @@ from accounts.models import Producer
 from django.views.generic import DetailView, ListView
 from django.shortcuts import get_object_or_404
 from .models import Product
-
 from django.db.models import Q
 import json
 
