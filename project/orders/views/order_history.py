d8fab14713c8eb5214de4c5ee094d4abc98c65cd
Oishik <pippal5536@gmail.com>
2026-03-18 21:11:40 +0000

    [Refactor] refactor order history filters to render all dropdown choices from view context

--- project/orders/views/order_history.py
+++ project/orders/views/order_history.py
@@ -1,7 +1,22 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
+from orders.models import Order
+
 
 @login_required
 def order_history_page(request):
-    return render(request, "orders/order_history.html")
+    recurring_choices = [
+        ("true", "Yes"),
+        ("false", "No"),
+    ]
+
+    return render(
+        request,
+        "orders/order_history.html",
+        {
+            "order_status_choices": Order.Status.choices,
+            "fulfilment_choices": Order.DeliveryOrCollection.choices,
+            "recurring_choices": recurring_choices,
+        },
+    )
