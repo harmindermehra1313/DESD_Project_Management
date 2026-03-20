c1feae07fc42d72fd5e5153213e12c1efd565427
Harminder Singh <Harminder2.Singh@live.uwe.ac.uk>
2026-03-20 13:38:22 +0000

    Fixed Errors

--- project/products/producer_urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('products/', views.producer_products, name='producer_products'),
-    path('products/<int:pk>/edit/', views.edit_producer_product, name='edit_producer_product'),
-    path('products/<int:pk>/cancel/', views.cancel_producer_product, name='cancel_producer_product'),
-]
