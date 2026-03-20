0b32a519b2e0abb973b7ca82cc497fa6292849af
Oishik <pippal5536@gmail.com>
2026-03-20 00:35:22 +0000

    [Fix(Product Detail Page)] Clicking back to product on product detail page shows error.

--- project/products/urls.py
+++ project/products/urls.py
@@ -4,6 +4,7 @@ from django.urls import path
 from . import views
 from .views import product_detail_page
 
+
 urlpatterns = [
     # path('', views.product_list, name='products_list'),
     path('add/', views.add_product, name='add_product'),
