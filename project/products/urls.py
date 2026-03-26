15407d482212b601f271c8164d1eb282bcb31d06
hj2-higgs <Hannah2.Higgs@live.uwe.ac.uk>
2026-03-26 21:04:38 +0000

    Hannah css updates (#58)
    * Updated css on created pages to use main styling. Fixed guest checkout bug.
    
    * Added new batches, update stock on existing batches and delete (soft) batches. I also fixed checkout so wholesale prices are only available to the correct roles (both display and back-end). Fixed some css issues noted in meeting.
    
    * Updated wholesale roles
    
    * Updated role logic for business
    
    * Fixed lots of little issues with JS.
    
    ---------
    
    Co-authored-by: HARMINDER SINGH <harmindermehra1313@gmail.com>

--- project/products/urls.py
+++ project/products/urls.py
@@ -3,7 +3,7 @@
 from django.urls import path
 from . import views
 from .views.views_main import add_product, product_view, producer_products, edit_producer_product, cancel_producer_product, ProductDetailView, product_detail_page
-from .views import reductions, api_reductions
+from .views import reductions, api_reductions, batch
 
 urlpatterns = [
     # path('', views.product_list, name='products_list'),
@@ -20,9 +20,13 @@ urlpatterns = [
     path("api/surplus/<int:pk>/create/", api_reductions.SurplusCreateAPI.as_view(), name="api_surplus_create"),
     path("api/surplus/<int:pk>/update/", api_reductions.SurplusUpdateAPI.as_view(), name="api_surplus_update"),
     path("api/surplus/<int:pk>/cancel/", api_reductions.SurplusCancelAPI.as_view(), name="api_surplus_cancel"),
-    
+    path("producer/products/<int:pk>/add-batch/",batch.add_batch,name="add_batch"),
+    path("producer/products/<int:pk>/reduce-batch/",batch.reduce_batch,name="reduce_batch"),
+    path("producer/products/<int:pk>/delete-batch/",batch.delete_batch,name="delete_batch"),
+
     #Joe
     # path('producer/', producer_products, name='producer_products'),
     path('producer/products/<int:pk>/edit/', edit_producer_product, name='edit_producer_product'),
     path('producer/products/<int:pk>/cancel/', cancel_producer_product, name='cancel_producer_product'),
+
 ]
