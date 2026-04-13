d95cbbb60668322377b218c3d4ec5491595cb268
hj2-higgs <Hannah2.Higgs@live.uwe.ac.uk>
2026-04-13 10:40:43 +0100

    Added notifications for low stock icons and messages. Added JS to handle front-end without reloading when stock updated (for low stock cases). Added notification centre on producer dashboard. Added updates for low stock thresholds. TBC: add email notifications once cluster set up.

--- project/home/urls.py
+++ project/home/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("", views.home, name="index"),
     path("dashboard/", views.dashboard, name="dashboard"),
     path("producer/", views.producer, name="producer"),
+    path("producer/notifications/mark-read/", views.mark_all_notifications_read, name="mark_all_notifications_read"),
 ]
--- project/home/views.py
+++ project/home/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from BRFN.decorators import admin_required, producer_required
-
-# Create your views here.
+from notifications.models import Notification
+from notifications.services.notifications import NotificationService
 
 def home(request):
     return render(request, "home/home.html")
@@ -12,4 +12,24 @@ def dashboard(request):
 
 @producer_required
 def producer(request):
-    return render(request, "home/producer.html")
+    producer = request.user.producer_profile
+
+    notifications = Notification.objects.filter(
+        user=request.user
+    ).order_by('-created_at')[:20] # latest 20
+
+    unread_count = Notification.objects.filter(
+        user=request.user,
+        read_at__isnull=True
+    ).count()
+    
+    return render(request, "home/producer.html", {
+        "notifications": notifications,
+        "unread_count": unread_count,
+    })
+
+@producer_required
+def mark_all_notifications_read(request):
+    if request.method == "POST":
+        NotificationService.mark_all_read(request.user)
+    return redirect('producer')
