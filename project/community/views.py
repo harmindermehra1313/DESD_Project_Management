487fdb863fef66b1cec6959a27923990edf8e02a
joemorton123 <joe2.morton@live.uwe.ac.uk>
2026-04-20 16:09:26 +0100

    added a contact us page

--- project/community/views.py
+++ project/community/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, "community/index.html")
+
+
+def contact_us(request):
+    context = {
+        "contact_phone": "0800 00 1066",
+        "contact_email": "BRFN@farmers.co.uk",
+        "contact_address": "Coldharbour Lane, Bristol, BS16 1QY",
+    }
+    return render(request, "community/contact_us.html", context)
