38a925dec535db467f2f0c82a8215db20d683e94
Oishik <pippal5536@gmail.com>
2026-05-03 01:01:18 +0100

    optimised detoxify logging

--- project/reviews/apps.py
+++ project/reviews/apps.py
@@ -1,5 +1,14 @@
+import logging
 import os
+import sys
+from threading import Lock
+
 from django.apps import AppConfig
+
+logger = logging.getLogger(__name__)
+
+_model_preload_lock = Lock()
+_model_preloaded = False
 
 
 class ReviewsConfig(AppConfig):
@@ -7,10 +16,26 @@ class ReviewsConfig(AppConfig):
     name = "reviews"
 
     def ready(self):
+        global _model_preloaded
+
+        if os.environ.get("PRELOAD_REVIEW_MODERATION") != "1":
+            return
+
+        if "runserver" not in sys.argv:
+            return
+
         if os.environ.get("RUN_MAIN") != "true":
             return
-        from reviews.services.moderation_service import get_model
-        
-        
 
-        get_model()
+        if _model_preloaded:
+            return
+
+        with _model_preload_lock:
+            if _model_preloaded:
+                return
+
+            from reviews.services.moderation_service import get_model
+
+            get_model()
+            _model_preloaded = True
+            logger.info("Review moderation model preloaded successfully.")
