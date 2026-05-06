6a09f0247f3b9c19129f1ef738d41593107758bf
Oishik <pippal5536@gmail.com>
2026-05-06 02:26:06 +0100

    Use polling to show new notifications without a full page reload

--- project/notifications/urls.py
+++ project/notifications/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+
 from . import views
 
-app_name = 'notifications'
+app_name = "notifications"
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("api/panel/", views.notification_panel_json, name="notification_panel_json"),
 ]
